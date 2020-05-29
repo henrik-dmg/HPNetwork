2d43d833609d7a7f0ec28ade9e08d4d6e8e7cde8
Henrik Panhans <henrik@panhans.dev>
2020-05-29 16:55:02 +0200

    making some tests

---
+++ Tests/HPNetworkTests/NetworkTests.swift
@@ -0,0 +1,22 @@
+@testable import HPNetwork
+import XCTest
+
+class NetworkTests: XCTestCase {
+
+    func testSimpleRequest() {
+        let expectation = XCTestExpectation(description: "fetched from server")
+
+        let request = DecodableRequest<EmptyStruct>(urlString: "https://ipapi.co/json")
+
+        Network.shared.dataTask(request) { result in
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 20)
+    }
+
+}
+
+struct EmptyStruct: Codable {
+
+}
