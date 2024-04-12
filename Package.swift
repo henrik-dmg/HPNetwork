1b30bca17cc60b92c2613e98cbf89f24c614b89f
Henrik Panhans <henrik@panhans.dev>
2024-04-12 10:55:27 +0200

    Hopefully fix documentation deploy
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// swift-format-ignore-file
+
 import PackageDescription
 
 let package = Package(
@@ -19,7 +21,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-http-types.git", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-http-types.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
