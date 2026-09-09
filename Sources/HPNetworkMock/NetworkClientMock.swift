b778c3806c3c93555634ff0bdd58074695d966f5
Henrik Panhans <henrik@panhans.dev>
2026-09-09 13:54:11 +0200

    fix error
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

--- Sources/HPNetworkMock/NetworkClientMock.swift
+++ Sources/HPNetworkMock/NetworkClientMock.swift
@@ -13,7 +13,7 @@ protocol MockedRequest<Request> {
 }
 
 /// A mockable network client.
-public final class NetworkClientMock: NetworkClientProtocol {
+public final class NetworkClientMock: NetworkClientProtocol, Sendable {
 
     // MARK: - Nested Types
 
